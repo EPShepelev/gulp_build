51a11361fc2776258bd43250702b75edcf906582
Шепелев Евгений <shepelev.e@ad1team.ru>
2022-04-20 12:44:37 +0300

    create plugin for img path resolving

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 import gulp from 'gulp'
 
 import { path } from './gulp/config/path.js'
+import { plugins } from './gulp/config/plugins.js'
 
 global.app = {
-  path: path,
-  gulp
+  path,
+  gulp,
+  plugins
 }
 
 import { copy } from './gulp/tasks/copy.js'
