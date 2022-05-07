fdc8fdfdb94f60a341167c63ffd81d2718c423cb
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2022-05-07 15:43:43 +0300

    add image task

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,11 +1,13 @@
-import replace from 'gulp-replace'
-import plumber from 'gulp-plumber'
-import notify from 'gulp-notify'
-import browserSync from 'browser-sync'
+import replace from "gulp-replace";
+import plumber from "gulp-plumber"; // обработка ошибок
+import notify from "gulp-notify";
+import browserSync from "browser-sync";
+import newer from "gulp-newer"; // проверяет изменилась ли картинка
 
 export const plugins = {
   replace,
   plumber,
   notify,
   browserSync,
-}
+  newer,
+};
