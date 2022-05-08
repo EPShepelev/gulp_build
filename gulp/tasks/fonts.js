f5b7e418b762262c6be20938cb6895331e622eb8
Evgeniy Shepelev <shepelev.ep@yandex.ru>
2022-05-08 10:08:00 +0300

    create font task

---
+++ gulp/tasks/fonts.js
@@ -0,0 +1,62 @@
+import fs from "fs";
+import fonter from "gulp-fonter";
+import ttf2woff2 from "gulp-ttf2woff2";
+
+export const otfToTtf = () => {
+  return app.gulp
+    .src(`${app.path.srcFolder}/fonts/*.otf`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "FONTS",
+          message: "Error: <%= error.message %>",
+        })
+      )
+    )
+    .pipe(
+      fonter({
+        formats: ["ttf"],
+      })
+    )
+    .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`));
+};
+
+export const ttfToWoff = () => {
+  return app.gulp
+    .src(`${app.path.srcFolder}/fonts/*.ttf`, {})
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "FONTS",
+          message: "Error: <%= error.message %>",
+        })
+      )
+    )
+    .pipe(
+      fonter({
+        formats: ["woff"],
+      })
+    )
+    .pipe(app.gulp.dest(`${app.path.build.fonts}`))
+    .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`, {}))
+    .pipe(ttf2woff2())
+    .pipe(app.gulp.dest(`${app.path.build.fonts}`));
+};
+
+export const fontStyle = () => {
+  let fontsFile = `${app.path.srcFolder}/scss/fonts.scss`;
+  fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
+    if (fontsFiles) {
+      if (!fs.existsSync(fontsFiles)) {
+        fs.writeFile(fontsFile, "", cb);
+        let newFileOnly;
+        for (let i = 0; i < fontsFiles.length; i++) {
+          let fontFileName = fontsFiles[i].split(".")[0];
+          if (newFileOnly !== fontFileName) {
+            // ДОДЕЛАТЬ!
+          }
+        }
+      }
+    }
+  });
+};
