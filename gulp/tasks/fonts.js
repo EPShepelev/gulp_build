ca557284e62e13175f82b1525f242ad4d839be59
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2022-07-28 17:19:42 +0300

    complete font task

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -53,10 +53,55 @@ export const fontStyle = () => {
         for (let i = 0; i < fontsFiles.length; i++) {
           let fontFileName = fontsFiles[i].split(".")[0];
           if (newFileOnly !== fontFileName) {
-            // ДОДЕЛАТЬ!
+            let fontName = fontFilename.split("-")[0]
+              ? fontFilename.split("-")[0]
+              : fontFilename;
+            let fontWeight = fontFilename.split("-")[1]
+              ? fontFilename.split("-")[1]
+              : fontFilename;
+            if (fontWeight.toLowerCase() === "thin") {
+              fontWeight = 100;
+            } else if (fontWeight.toLowerCase() === "extralight") {
+              fontWeight = 200;
+            } else if (fontWeight.toLowerCase() === "light") {
+              fontWeight = 300;
+            } else if (fontWeight.toLowerCase() === "medium") {
+              fontWeight = 400;
+            } else if (fontWeight.toLowerCase() === "semibold") {
+              fontWeight = 600;
+            } else if (fontWeight.toLowerCase() === "bold") {
+              fontWeight = 700;
+            } else if (
+              fontWeight.toLowerCase() === "extrabold" ||
+              fontWeight.toLowerCase() === "heavy"
+            ) {
+              fontWeight = 800;
+            } else if (fontWeight.toLowerCase() === "black") {
+              fontWeight = 900;
+            } else {
+              fontWeight = 400;
+            }
+            fs.appendFile(
+              fontsFile,
+              `@font-face {
+              font-family: ${fontName};
+              font-display: swap;
+              src: url('../fonts/${fontFileName}.woff2') format('woff2'), url('../fonts/${fontFileName}.woff') format('woff');
+              font-weight: ${fontWeight};
+              font-style: normal;
+            }\r\n`,
+              cb
+            );
+            newFileOnly = fontFileName;
           }
         }
+      } else {
+        console.log(
+          "file scss/fonts.scss already exist. If you want to update - delete it first"
+        );
       }
     }
   });
+  return app.gulp.src(`${app.path.srcFolder}`);
+  function cb() {}
 };
